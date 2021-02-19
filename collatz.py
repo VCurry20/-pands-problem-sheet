f52e9b90125cfde38dfc5b3fe3e153e958991a27
Curry <G00074924@gmit.ie>
2021-02-19 20:23:08 +0000

    collatz.py

---
+++ collatz.py
@@ -0,0 +1,13 @@
+# Week 4 Task
+
+# Write a program that asks the user to input a positive integer 
+# outputs successive values 
+
+# @ each step calculate the next value by taking the current value and if even - divide by 2 / if odd multiple by three and add one
+
+# Please enter a positive integer:  10
+# result: 10, 5, 16, 8, 4, 2, 1
+
+
+# rules like grading ?
+# loops until it gets to an end point
