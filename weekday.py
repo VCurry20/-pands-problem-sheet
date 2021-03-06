e524bd24c9d7870f61370409710fb7151864e87f
Curry <G00074924@gmit.ie>
2021-03-06 13:06:12 +0000

    weekday.py

---
+++ weekday.py
@@ -0,0 +1,12 @@
+# Week 5 Tasks
+
+# Write a program that outputs whether or not today is a weekday
+# Should out put # Yes, unfortunately today is a weekday or It is the weekend, Yay!
+
+## For loops and list of days of the week
+## Array / list of days?
+
+# daysofWeek = (Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday)
+# weekend = last two of this list - 5, 6
+
+# if when x do a and if not do b
