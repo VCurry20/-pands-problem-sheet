18a2b0fbadf89317b3e2323ae33ff27540c90711
Curry <G00074924@gmit.ie>
2021-04-07 22:45:36 +0100

    week 7 completed

--- es.py
+++ es.py
@@ -11,11 +11,22 @@
 # read out a number - the total count of E's
 # Capital / Not Capital isnt specified
 
-## with open("GreatExpectations.TXT","r") as f:
-##  data = f.read (e)
-## print (count(data))    ## or is it len?
+import sys                          # 1. Import System 
 
-
+with open (sys.argv[1]) as f:       # 2. Open the file as inputed through the command line 
+    data = f.read()                 # 3. Set data as the file contents
+    freq = data.count("e")          # 4. Set Frequency as the count of "e" in the file
+    print (freq)                    # 5. Print Frequency
 
 
 # Reference 1. https://www.gutenberg.org/files/1400/1400-0.txt
+# Reference 2. https://stackoverflow.com/questions/22694244/counting-specific-letters-or-symbols-in-a-text-file-in-python
+# Reference 3. https://pythonexamples.org/python-count-number-of-characters-in-text-file/
+# Reference 4. https://www.geeksforgeeks.org/count-the-number-of-times-a-letter-appears-in-a-text-file-in-python/
+# Reference 5. https://realpython.com/python-command-line-arguments/
+# Reference 6. https://www.tutorialspoint.com/python/python_command_line_arguments.htm
+# Reference 7. https://docs.python.org/3/library/sys.html
+
+
+
+
---
+++ stuff.py
@@ -0,0 +1,28 @@
+
+
+
+
+
+## with open("GreatExpectations.TXT","r") as f:
+##  data = f.read (e)
+## print (count(data))    
+
+#myfile = open ("greatExpectations.txt", "r")
+
+#with open("testdata.txt","rt") as f:          # print all from a file ( has a limit not sure how much)
+ #   data = f.read()                           # we created this file manually to trial from
+  #  print (data)
+
+#def countletters
+#readFile = 
+
+#with open ("greatExpectations.txt", "rt") as f:
+ #   data = f.read ("e")
+    
+    
+
+#print(data)
+
+fileName = input ("Enter a file: ")
+
+
